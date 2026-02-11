64c213e05d1c9e38c5e7aa8ef5bc1a2d0bbf2ba0
Guilherme Calixto Campos <gcalixtocampos16@gmail.com>
2026-02-11 15:34:49 -0300

    Finalizado interacoes e functions do index

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
-const tarefas = document.querySelector('#tarefas')
-const nenhumaTarefa = document.querySelector('#nenhuma-tarefa')
-const tarefa = document.querySelector('#tarefa')
-const btnAddTarefa = document.querySelector('#btn-add-tarefa')
-const btnSaveTarefa = document.querySelector('#btn-save-tarefa')
+const botoesAdd = document.querySelectorAll('.btn-add-tarefa')
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || {
+    segunda: [],
+    terca: [],
+    quarta: [],
+    quinta: [],
+    sexta: [],
+    sabado: [],
+    domingo: []
+}
 
-btnAddTarefa.addEventListener('click',() => {
-    tarefa.classList.toggle('hidden')
-    btnSaveTarefa.classList.toggle('hidden')
-    
+botoesAdd.forEach(botao => {
+
+    botao.addEventListener('click', () => {
+
+        const card = botao.closest('.card-dia')
+        const textarea = card.querySelector('.input-tarefa')
+        const btnSave = card.querySelector('.btn-save-tarefa')
+        const ul = card.querySelector('.lista-tarefas')
+        const msg = card.querySelector('.msg-vazia')
+
+        const dia = card.dataset.dia
+        console.log('Dia clicado:', dia)
+
+        textarea.classList.toggle('hidden')
+        btnSave.classList.toggle('hidden')
+
+        // botão salvar
+        btnSave.onclick = () => {
+
+            const tarefa = textarea.value.trim()
+            if (!tarefa) return
+
+            const li = document.createElement('li')
+            li.textContent = tarefa
+            li.classList.add('text-gray-400', 'text-sm')
+
+            ul.appendChild(li)
+
+            
+
+            tarefas[dia].push(tarefa)
+
+            localStorage.setItem("tarefas", JSON.stringify(tarefas))
+            textarea.value = ''
+            textarea.classList.add('hidden')
+            btnSave.classList.add('hidden')
+        }
+
+        
+
+    })
+
 })
+function carregarTarefas() {
+
+    const cards = document.querySelectorAll('.card-dia')
+
+    cards.forEach(card => {
+
+        const dia = card.dataset.dia
+        const ul = card.querySelector('.lista-tarefas')
+        const msg = card.querySelector('.msg-vazia')
+
+        ul.innerHTML = ''
+
+        if (tarefas[dia].length === 0) {
+            msg.classList.remove('hidden')
+        } else {
+            msg.classList.add('hidden')
+
+            tarefas[dia].forEach(tarefa => {
+                const li = document.createElement('li')
+                li.textContent = tarefa
+                li.classList.add('text-gray-400', 'text-sm')
+                ul.appendChild(li)
+            })
+        }
+
+    })
+}
+
+carregarTarefas()
+
